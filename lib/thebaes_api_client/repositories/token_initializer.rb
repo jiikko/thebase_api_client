621cd3115d15197b04bec8bb8521e8cec87302cd
jiikko <n905i.1214@gmail.com>
2020-06-13 19:59:44 +0900

    tokenを作成する処理をクラスに切り出す

---
+++ lib/thebaes_api_client/repositories/token_initializer.rb
@@ -0,0 +1,54 @@
+module ThebaesApiClient::Repositories::TokenInitializer
+  attr_accessor :thebase_account
+
+  def initialize(thebase_account)
+    oauth2_client = TheBaseIn::OAuthClient.new(
+      thebase_account.access_token,
+      thebase_account.refresh_token,
+      thebase_account.expires_at,
+    )
+
+    refresh! if thebase_account.expired?
+    begin
+      yield(oauth2_client)
+    rescue OAuth2::Error => e
+      # アクセストークンが無効になった時にrefreshAPIのレスポンスで判断する
+      # TODO refresh後はoauth2_clientを作り直さないといけない
+      refresh! if invalid_access_token?(e)
+      raise
+    end
+  end
+
+  def expired?
+    thebase_account.expires_at.feature?
+  end
+
+  def revoke!
+    Rails.logger.info("#{user.slice(:id, :display_name, :email)}のBASE連携を無効化しました")
+    thebase_account.update!(revoked_at: Time.current)
+  end
+
+  def revoked?(e)
+    # BASE連携解除すると`invalid_request: 不正なリフレッシュトークンです。`が返ってくる
+    # https://github.com/baseinc/api-docs/blob/master/base_api_v1_oauth_refresh_token.md
+    /invalid_request/ =~ e.code && \
+      /不正なリフレッシュトークンです/ =~ e.description
+  end
+
+  # https://github.com/monolink-corp/monolink-server/issues/3247
+  def deleted_account?(e)
+    /access_denied/ =~ e.code && \
+      /ユーザーが存在しません/ =~ e.description
+  end
+
+  def invalid_access_token?(e)
+    /invalid_request/ =~ e.code && \
+      /アクセストークンが無効です/ =~ e.description
+  end
+
+  # https://github.com/monolink-corp/monolink-server/issues/3459
+  def baned?(e)
+    /access_denied/ =~ e.code && \
+      /利用が制限されているユーザーです/ =~ e.description
+  end
+end
--- lib/thebaes_api_client/service.rb
+++ lib/thebaes_api_client/service.rb
@@ -3,14 +3,10 @@ class ThebaesApiClient::Service
 
   include ThebaesApiClient::Auth
 
-  attr_accessor :thebase_account, :oauth2_client
+  attr_accessor :thebase_account
 
   def initialize(thebase_account)
-    @oauth2_client = TheBaseIn::OAuthClient.new(
-      thebase_account.access_token,
-      thebase_account.refresh_token,
-      thebase_account.expires_at,
-    )
+    self.thebase_account = thebase_account
   end
 
   def self.get_tokens(thebase_authorize_code)
@@ -74,47 +70,7 @@ class ThebaesApiClient::Service
 
   private
 
-  def call_api
-    refresh! if thebase_account.expired?
-    begin
-      yield
-    rescue OAuth2::Error => e
-      # アクセストークンが無効になった時にrefreshAPIのレスポンスで判断する
-      refresh! if invalid_access_token?(e)
-      raise
-    end
-  end
-
-  def expired?
-    thebase_account.expires_at.feature?
-  end
-
-  def revoke!
-    Rails.logger.info("#{user.slice(:id, :display_name, :email)}のBASE連携を無効化しました")
-    thebase_account.update!(revoked_at: Time.current)
-  end
-
-  def revoked?(e)
-    # BASE連携解除すると`invalid_request: 不正なリフレッシュトークンです。`が返ってくる
-    # https://github.com/baseinc/api-docs/blob/master/base_api_v1_oauth_refresh_token.md
-    /invalid_request/ =~ e.code && \
-      /不正なリフレッシュトークンです/ =~ e.description
-  end
-
-  # https://github.com/monolink-corp/monolink-server/issues/3247
-  def deleted_account?(e)
-    /access_denied/ =~ e.code && \
-      /ユーザーが存在しません/ =~ e.description
-  end
-
-  def invalid_access_token?(e)
-    /invalid_request/ =~ e.code && \
-      /アクセストークンが無効です/ =~ e.description
-  end
-
-  # https://github.com/monolink-corp/monolink-server/issues/3459
-  def baned?(e)
-    /access_denied/ =~ e.code && \
-      /利用が制限されているユーザーです/ =~ e.description
+  def call_api(&block)
+    ThebaesApiClient::Repositories::TokenInitializer.new(thebase_account, &block)
   end
 end
